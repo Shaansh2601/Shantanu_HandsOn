28a8054e00c340f07b73e6633756105d3db55bb7
Shantanu Kumar <shantanu.singh@cognizant.com>
2021-03-02 21:53:26 +0530

    Adding Handson

---
+++ WindowsFormsApplication/WindowsFormsApplication/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿
+namespace WindowsFormsApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.Add = new System.Windows.Forms.RadioButton();
+            this.Subtract = new System.Windows.Forms.RadioButton();
+            this.Multiply = new System.Windows.Forms.RadioButton();
+            this.Divide = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(415, 96);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(415, 141);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // Add
+            // 
+            this.Add.AutoSize = true;
+            this.Add.Location = new System.Drawing.Point(415, 190);
+            this.Add.Name = "Add";
+            this.Add.Size = new System.Drawing.Size(44, 17);
+            this.Add.TabIndex = 2;
+            this.Add.TabStop = true;
+            this.Add.Text = "Add";
+            this.Add.UseVisualStyleBackColor = true;
+            this.Add.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // Subtract
+            // 
+            this.Subtract.AutoSize = true;
+            this.Subtract.Location = new System.Drawing.Point(415, 213);
+            this.Subtract.Name = "Subtract";
+            this.Subtract.Size = new System.Drawing.Size(65, 17);
+            this.Subtract.TabIndex = 3;
+            this.Subtract.TabStop = true;
+            this.Subtract.Text = "Subtract";
+            this.Subtract.UseVisualStyleBackColor = true;
+            // 
+            // Multiply
+            // 
+            this.Multiply.AutoSize = true;
+            this.Multiply.Location = new System.Drawing.Point(415, 236);
+            this.Multiply.Name = "Multiply";
+            this.Multiply.Size = new System.Drawing.Size(60, 17);
+            this.Multiply.TabIndex = 4;
+            this.Multiply.TabStop = true;
+            this.Multiply.Text = "Multiply";
+            this.Multiply.UseVisualStyleBackColor = true;
+            // 
+            // Divide
+            // 
+            this.Divide.AutoSize = true;
+            this.Divide.Location = new System.Drawing.Point(415, 259);
+            this.Divide.Name = "Divide";
+            this.Divide.Size = new System.Drawing.Size(55, 17);
+            this.Divide.TabIndex = 5;
+            this.Divide.TabStop = true;
+            this.Divide.Text = "Divide";
+            this.Divide.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(415, 294);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 29);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Divide);
+            this.Controls.Add(this.Multiply);
+            this.Controls.Add(this.Subtract);
+            this.Controls.Add(this.Add);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.RadioButton Add;
+        private System.Windows.Forms.RadioButton Subtract;
+        private System.Windows.Forms.RadioButton Multiply;
+        private System.Windows.Forms.RadioButton Divide;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
