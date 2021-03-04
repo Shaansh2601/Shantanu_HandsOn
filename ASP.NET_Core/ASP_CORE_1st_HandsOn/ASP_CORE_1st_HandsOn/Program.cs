1ca55b879d90c08d3c94011791e69565c94e2fcb
Shantanu Kumar <shantanu.singh@cognizant.com>
2021-03-04 09:07:52 +0530

    HandsOn

---
+++ ASP.NET_Core/ASP_CORE_1st_HandsOn/ASP_CORE_1st_HandsOn/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ASP_CORE_1st_HandsOn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
