db02ebd7bf4e53cbf93617a6083dec1daf694201
Shantanu Kumar <shantanu.singh@cognizant.com>
2021-03-18 10:32:33 +0530

    HAndson

---
+++ Design Principle HandsOn/BuilderPatternHandsOn_4/BuilderPatternHandsOn/ConcreteAdultPackage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuilderPatternHandsOn
+{
+    class ConcreteAdultPackage:Builder
+    {
+        Product p = new Product();
+        public void BuildSavories()
+        {
+            p.Add("2 Savories");
+        }
+
+        public void BuildSweet()
+        {
+            p.Add("2 Sweets");
+        }
+
+        public Product GetResults()
+        {
+            return p;
+        }
+    }
+}
